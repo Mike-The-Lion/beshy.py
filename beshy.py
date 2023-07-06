60741685e3d179db3442d8bb8765c226ae6dec1c
Mike-The-Lion <iammaykhel@gmail.com>
2023-07-06 15:13:02 +0400

    minor change

--- beshy.py
+++ beshy.py
@@ -7,10 +7,10 @@ def replace_spaces_with_emoticons(text):
 
 st.title("My Beshie")
 
-user_input = st.text_input("Enter your text here:")
+user_input = st.text_input("Enter your text here beshie:")
 
 if user_input:
     result = replace_spaces_with_emoticons(user_input)
     clipboard.copy(result)  # This will copy the result to your clipboard
     st.write(result)
-    st.write("The result has been copied to your clipboard.")
+    st.write("Beshie, the result has been copied to your clipboard.")
