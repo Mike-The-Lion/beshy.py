dd2bf06fa89716a43f07a0ca64d478a41d13f745
Mike-The-Lion <iammaykhel@gmail.com>
2023-07-06 15:36:22 +0400

    minor change

--- beshy.py
+++ beshy.py
@@ -1,38 +1,13 @@
 import streamlit as st
-from streamlit import cli as stcli
-import streamlit.components.v1 as components
-import argparse
-import sys
 
-def main():
-    st.title('Beshie! 🤸🏼🤸🏼🤸🏼')
-    
-    user_input = st.text_input("Input your text here:")
+def replace_spaces_with_emoticons(text):
+    emoticon = "🤸🏼🤸🏼"  # You can change the emoticon to anything you like
+    return text.replace(" ", emoticon)
 
-    # Define the emoticon you want to replace spaces with
-    emoticon = "🤸🏼"
+st.title("Beshie! 🤸🏼")
 
-    # Replace spaces in the input string with the emoticon
-    output_text = user_input.replace(" ", emoticon)
+user_input = st.text_input("Enter your text here:")
 
-    st.write(output_text)
-
-    # Create a button for the user to copy the output text
-    copy_button = components.html("""
-        <button class="copy-button" data-clipboard-text="{}">
-            Copy to clipboard
-        </button>
-        <script>
-        new ClipboardJS('.copy-button');
-        </script>
-        """.format(output_text))
-
-    if copy_button:
-        st.write('Text copied to clipboard!')
-
-if __name__ == "__main__":
-    if st._is_running_with_streamlit:
-        main()
-    else:
-        sys.argv = ["streamlit", "run", sys.argv[0]]
-        sys.exit(stcli.main())
+if user_input:
+    result = replace_spaces_with_emoticons(user_input)
+    st.write(result)
