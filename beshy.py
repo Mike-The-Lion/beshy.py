8d0dcc7af7526914eeb1c7b2d7ef09e71b1d2685
Mike-The-Lion <iammaykhel@gmail.com>
2023-07-06 15:06:47 +0400

    change code

--- beshy.py
+++ beshy.py
@@ -1,23 +1,13 @@
 import streamlit as st
-import emoji
 
-# Create a title
-st.title('Space to Emoji Converter')
+def replace_spaces_with_emoticons(text):
+    emoticon = "🤸🏼🤸🏼"  # You can change the emoticon to anything you like
+    return text.replace(" ", emoticon)
 
-# Add a text input field to the sidebar
-user_input = st.sidebar.text_input("Enter a sentence")
+st.title("Beshie")
 
-# Add a selectbox to the sidebar
-selected_emoji = st.sidebar.selectbox(
-    'Select an emoji',
-    (':grinning:', ':winking_face:', ':star-struck:', ':thinking_face:', ':face_with_hand_over_mouth:')
-)
+user_input = st.text_input("Enter your text here:")
 
-# Convert the chosen emoji to unicode
-unicode_emoji = emoji.emojize(selected_emoji, use_aliases=True)
-
-# Replace spaces with the selected emoji
-converted_sentence = user_input.replace(' ', unicode_emoji)
-
-# Write the converted sentence to the main page
-st.write(converted_sentence)
+if user_input:
+    result = replace_spaces_with_emoticons(user_input)
+    st.write(result)
