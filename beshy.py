a7aeaa94104c6612e45404e2e3eb1406962a46df
Mike-The-Lion <iammaykhel@gmail.com>
2023-07-06 15:19:40 +0400

    minor change

--- beshy.py
+++ beshy.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import clipboard
+import pyperclip
 
 def replace_spaces_with_emoticons(text):
     emoticon = "🤸🏼🤸🏼"  # You can change the emoticon to anything you like
@@ -11,6 +11,6 @@ user_input = st.text_input("Enter your text here beshie:")
 
 if user_input:
     result = replace_spaces_with_emoticons(user_input)
-    clipboard.copy(result)  # This will copy the result to your clipboard
+    pyperclip.copy(result)  # This will copy the result to your clipboard
     st.write(result)
     st.write("Beshie, the result has been copied to your clipboard.")
