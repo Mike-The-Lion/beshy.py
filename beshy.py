17d54665ff0e953258c5db9d0eee6ff9bf962b81
Mike-The-Lion <iammaykhel@gmail.com>
2023-07-06 15:26:10 +0400

    change code

--- beshy.py
+++ beshy.py
@@ -1,16 +1,38 @@
 import streamlit as st
-import pyperclip
+from streamlit import cli as stcli
+import streamlit.components.v1 as components
+import argparse
+import sys
 
-def replace_spaces_with_emoticons(text):
-    emoticon = "🤸🏼🤸🏼"  # You can change the emoticon to anything you like
-    return text.replace(" ", emoticon)
+def main():
+    st.title('Beshie! 🤸🏼🤸🏼🤸🏼')
+    
+    user_input = st.text_input("Input your text here:")
 
-st.title("My Beshie")
+    # Define the emoticon you want to replace spaces with
+    emoticon = "🤸🏼"
 
-user_input = st.text_input("Enter your text here beshie:")
+    # Replace spaces in the input string with the emoticon
+    output_text = user_input.replace(" ", emoticon)
 
-if user_input:
-    result = replace_spaces_with_emoticons(user_input)
-    pyperclip.copy(result)  # This will copy the result to your clipboard
-    st.write(result)
-    st.write("Beshie, the result has been copied to your clipboard.")
+    st.write(output_text)
+
+    # Create a button for the user to copy the output text
+    copy_button = components.html("""
+        <button class="copy-button" data-clipboard-text="{}">
+            Copy to clipboard
+        </button>
+        <script>
+        new ClipboardJS('.copy-button');
+        </script>
+        """.format(output_text))
+
+    if copy_button:
+        st.write('Text copied to clipboard!')
+
+if __name__ == "__main__":
+    if st._is_running_with_streamlit:
+        main()
+    else:
+        sys.argv = ["streamlit", "run", sys.argv[0]]
+        sys.exit(stcli.main())
