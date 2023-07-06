bfe8b57268bdad1807d003629a459d643d1fce4e
Mike-The-Lion <iammaykhel@gmail.com>
2023-07-06 15:11:13 +0400

    added clipboard

--- beshy.py
+++ beshy.py
@@ -1,13 +1,16 @@
 import streamlit as st
+import clipboard
 
 def replace_spaces_with_emoticons(text):
     emoticon = "🤸🏼🤸🏼"  # You can change the emoticon to anything you like
     return text.replace(" ", emoticon)
 
-st.title("Beshie")
+st.title("My Beshie")
 
 user_input = st.text_input("Enter your text here:")
 
 if user_input:
     result = replace_spaces_with_emoticons(user_input)
+    clipboard.copy(result)  # This will copy the result to your clipboard
     st.write(result)
+    st.write("The result has been copied to your clipboard.")
